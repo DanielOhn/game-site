47bcce6da5690607487bb151d7b292ec7444241a
Daniel Ohn <ohndaniel@gmail.com>
2020-02-17 17:38:03 -0700

    Add game component.

---
+++ src/components/Game.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+interface Props {
+  name: string
+  desc: string
+}
+
+function Game(props: Props): JSX.Element {
+  return (
+    <div className="game">
+      <h2>{props.name}</h2>
+      <p>{props.desc}</p>
+    </div>
+  )
+}
+
+export default Game
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,13 @@
 import * as React from "react"
 import "../styles/App.css"
 
-import Hello from './Hello'
+import Hello from "./Hello"
+
+import Game from "../components/Game"
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Hello name="world" />
     </div>
   )
