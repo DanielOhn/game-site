1331bafc675466c1de012d0ee8b6dcc615a3ec43
Daniel Ohn <ohndaniel@gmail.com>
2020-02-21 14:21:59 -0700

    Add basic layout for website with placeholdes.

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
 import "../styles/App.css"
 
+import image from "../images/pong.png"
+
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
 
 import Landing from "./Landing"
@@ -11,25 +13,36 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/pong">Pong</Link>
-              </li>
-            </ul>
-          </nav>
-          <Switch>
-            <Route path="/" exact render={() => <Landing />} />
-            <Route
-              path="/pong"
-              exact
-              render={() => <Game name="Pong" desc="Classic game of Pong." />}
-            />
-          </Switch>
+        <div className="header">
+          <h1 className="title">
+            <Link to="/">Ohnage Games</Link>
+          </h1>
+        </div>
+        <div className="content">
+          <div className="body">
+            <Switch>
+              <Route path="/" exact render={() => <Landing />} />
+              <Route
+                path="/pong"
+                exact
+                render={() => <Game name="Pong" desc="Classic game of Pong." />}
+              />
+            </Switch>
+          </div>
+          <div className="games">
+            <div className="box">
+              <img src={image} alt="pong bois"></img>
+              <Link to="/pong">Pong</Link>
+            </div>
+            <div className="box">
+              <img src="http://placekitten.com/g/200/200" alt="cat"></img>
+              <Link to="/">Cat!</Link>
+            </div>
+            <div className="box">
+              <img src="http://placekitten.com/g/200/200" alt="cat"></img>
+              <Link to="/">Cat Again...</Link>
+            </div>
+          </div>
         </div>
       </Router>
     </div>
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,11 +3,12 @@ import React from "react"
 let Landing = (): JSX.Element => {
   return (
     <div>
-      <h1>Welcome to Ohnage Games!</h1>
+      <h2>Welcome!</h2>
       <p>
-        This website is used to upload various PixiJS games that I make in my
-        free time. Any feedback is appreciated!
+        This website is used to upload various games that I make in my free
+        time.
       </p>
+      <p>Any feedback is appreciated!</p>
       <h3>Latest Changes</h3>
       <ul>
         <li>Added Pong</li>
