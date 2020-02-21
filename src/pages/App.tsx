ac923ace28550edd94fb2e0b33e404a5c71dd0bb
Daniel Ohn <ohndaniel@gmail.com>
2020-02-20 22:51:01 -0700

    Add routers for landing and pong.

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
 import "../styles/App.css"
 
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
+
 import Landing from "./Landing"
 
 import Game from "../components/Game"
@@ -8,8 +10,28 @@ import Game from "../components/Game"
 function App() {
   return (
     <div className="App">
-      <Landing />
-      <Game name="Pong" desc="A simple, classic game of Pong." />
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/pong">Pong</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/" exact render={() => <Landing />} />
+            <Route
+              path="/pong"
+              exact
+              render={() => <Game name="Pong" desc="Classic game of Pong." />}
+            />
+          </Switch>
+        </div>
+      </Router>
     </div>
   )
 }
