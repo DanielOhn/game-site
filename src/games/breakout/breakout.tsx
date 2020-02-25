6c53928427bdb31aab9b88fb969685b788e6de20
Daniel Ohn <ohndaniel@gmail.com>
2020-02-24 18:03:35 -0700

    Add new side bar image.  Redoing collision.  Change walls to from sprites to graphics.

--- src/games/breakout/breakout.tsx
+++ src/games/breakout/breakout.tsx
@@ -1,15 +1,17 @@
 import * as React from "react"
 import * as PIXI from "pixi.js"
 
-import bar from "./images/bar.png"
+import bar from "./images/side_bar.png"
 import ballImg from "./images/ball.png"
-import Game from "../../components/Game"
+// import Game from "../../components/Game" (Futuer, Make Library for all 2D Math Code, collision, keyboard, reusable stuff)
 
 interface MovingObject extends PIXI.Sprite {
   vx?: number
   vy?: number
+  hp?: number
   lives?: number
   score?: number
+  dock?: boolean
 }
 
 function Breakout(): JSX.Element {
@@ -23,14 +25,17 @@ function Breakout(): JSX.Element {
   const loader = PIXI.Loader.shared
   const sprite = PIXI.Sprite
 
+  let stage: PIXI.Container
+
   let player: MovingObject
-  let balls: MovingObject[] = []
+  let ball: MovingObject
+  // let balls: MovingObject[] = []
 
-  // let bricks = []
-  // let walls = []
+  let bricks: MovingObject[] = []
+  let walls: PIXI.Graphics[] = []
 
-  // let row,
-  //   col = 5
+  let row = 5
+  let col = 5
 
   let left = keyboard("a")
   let right = keyboard("d")
@@ -55,7 +60,7 @@ function Breakout(): JSX.Element {
     let bar_texture = loader.resources.bar.texture
     let ball_texture = loader.resources.ball.texture
 
-    let stage = new PIXI.Container()
+    stage = new PIXI.Container()
     app.stage.addChild(stage)
 
     stage.x = app.screen.width / 2
@@ -63,6 +68,8 @@ function Breakout(): JSX.Element {
 
     initPlayer(stage, bar_texture)
     initBall(stage, ball_texture)
+    initWalls(stage, bar_texture)
+    initBricks(stage, bar_texture)
 
     left.press = () => {
       player.vx = -1
@@ -80,6 +87,10 @@ function Breakout(): JSX.Element {
       player.vx = 0
     }
 
+    space.press = () => {
+      if (ball.dock === true) ball.dock = false
+    }
+
     app.ticker.add(delta => game(delta))
   }
 
@@ -88,34 +99,135 @@ function Breakout(): JSX.Element {
     stage.addChild(player)
 
     player.y = 250
-    player.rotation = Math.PI / 2
     player.anchor.set(0.5)
 
     player.vx = 0
   }
 
   function initBall(stage: PIXI.Container, texture: PIXI.Texture): void {
-    let ball: MovingObject
     ball = new sprite(texture)
 
     ball.y = 225
-    ball.x = -8
+    ball.x = -6
 
     ball.vy = 0
     ball.vx = 0
 
-    balls.push(ball)
+    ball.anchor.set(0.5)
+
+    ball.dock = true
+
+    // balls.push(ball)
 
     stage.addChild(ball)
   }
 
+  function initWalls(stage: PIXI.Container, texture: PIXI.Texture): void {
+    let leftWall = new PIXI.Graphics()
+    let rightWall = new PIXI.Graphics()
+    let midWall = new PIXI.Graphics()
+
+    leftWall.beginFill(0x909090)
+    leftWall.drawRect(-400, -300, 50, 600)
+
+    rightWall.beginFill(0x909090)
+    rightWall.drawRect(350, -300, 50, 600)
+
+    midWall.beginFill(0x909090)
+    midWall.drawRect(-350, -300, 700, 50)
+
+    stage.addChild(leftWall)
+    stage.addChild(rightWall)
+    stage.addChild(midWall)
+
+    // walls.push(graphics)
+  }
+
+  function initBricks(stage: PIXI.Container, texture: PIXI.Texture): void {
+    for (let i = 0; i < row; i++) {
+      for (let j = 0; j < col; j++) {
+        let brick: MovingObject
+        brick = new sprite(texture)
+
+        stage.addChild(brick)
+
+        brick.x = (-row / 2) * 64 + 32 + 66 * i
+        brick.y = (col / 2) * 16 + -18 * j
+        brick.anchor.set(0.5)
+
+        brick.hp = 3
+        brick.tint = 0x355890
+        bricks.push(brick)
+      }
+    }
+  }
+
   function game(delta: number): void {
     let speed = delta * 5
+    let ball_speed = delta * 4
 
+    for (let wall of walls) {
+      if (checkCollision(player, wall)) {
+        if (player.vx) {
+          if (player.vx < 1 || player.vx > 1) {
+            player.vx = 0
+          }
+        }
+      }
+
+      if (checkCollision(ball, wall)) {
+        if (ball.vy) ball.vy = ball.vy * -1
+      }
+    }
+
+    // if (checkCollision(ball, player)) {
+    //   if (ball.vy) ball.vy *= -1
+    // }
+
+    if (ball.dock === true) {
+      if (player.vx) ball.x += player.vx * speed
+    } else if (ball.dock === false) {
+      send_ball()
+    }
+
+    if (ball.vx) ball.x += ball_speed * ball.vx
+    if (ball.vy) ball.y += ball_speed * ball.vy
     if (player.vx) player.x += speed * player.vx
   }
 
-  function keyboard(value: string) {
+  interface Point {
+    x: number
+    y: number
+  }
+
+  function send_ball(): void {
+    ball.vx = player.vx
+    ball.vy = -1
+    ball.dock = undefined
+  }
+
+  function checkCollision(a: PIXI.Sprite, b: PIXI.Graphics): boolean {
+    let yA = a.y - a.height / 2
+    let xA = a.x - a.width / 2
+
+    let finalX_A = xA
+    let finalY_A = yA
+
+    let yB = b.y - b.height / 2
+    let xB = b.x - b.width / 2
+
+    let finalX_B = xB
+    let finalY_B = yB
+
+    return (
+      finalX_A + a.width > finalX_B &&
+      finalX_A < finalX_B + b.width &&
+      finalY_A + a.height > finalY_B &&
+      finalY_A < finalY_B + b.height
+    )
+  }
+
+  function keyboard(value: string): any {
     let key = {
       value: value,
       isDown: false,
